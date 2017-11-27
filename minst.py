5ab76d7cb203b410402c9d3756f290f6ccf47d6e
Brien Premachandiran <premachb@gmail.com>
2017-11-27 02:23:32 -0500

    BP - Exploring both Andrew NG and 3blue1brown's courses on Neural Nets. Started off some Jupyter notebooks and took notes

---
+++ minst.py
@@ -0,0 +1,14 @@
+# Network takes as an input a vector of 784 pixel values.
+# The network outputs an vector of 10 values representing the likeliness of each digit.
+# The network is parameterized using weights representing the importance of each connection in the network.
+
+#
+#
+#
+#
+#
+
+from pandas import *
+def initialize_network():
+
+
